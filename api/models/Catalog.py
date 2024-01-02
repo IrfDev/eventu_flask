39ac836aa4d538c3ca587cad1d3aac56bc4eb993
Irving Suarez <irving.dev@icloud.com>
2024-01-02 01:28:35 -0600

    [DB]: Defining models

--- api/models/Catalog.py
+++ api/models/Catalog.py
@@ -1,3 +1,28 @@
 """
 Models for products and stock
 """
+from sqlalchemy import DateTime, Integer, String, ForeignKey
+from sqlalchemy_utils import EmailType
+from sqlalchemy.orm import mapped_column
+
+from ...db import db
+
+
+class Prouct(db.Model):
+    """Products"""
+
+    id = mapped_column(Integer, primary_key=True)
+    name = ""
+    description = ""
+    price = ""
+    discount = ""
+    category = ""
+    images = ""
+    feature_image = ""
+    type = ""
+    brand = ""
+    variables = ""
+
+
+class Product_Variable(db.Model):
+    """Variables for products"""
